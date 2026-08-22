7a73efe393b31e5aeadfb6dcbe358ecbd9297f2a
Bhagya1404 <154546955+Bhagya1404@users.noreply.github.com>
2026-08-23 00:21:06 +0530

    Delete src directory

--- src/pages/Profile.jsx
+++
@@ -1,5 +0,0 @@
-function Profile() {
-  return <h1 className="page">Profile Page</h1>;
-}
-
-export default Profile;
--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "https://quickbite-api-3ptx.onrender.com/api",
-});
-
-export default API;
